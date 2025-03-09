6d2f9c44f9abda7de306fd397d6e0f18e234c986
ggdubez <fwzanali911@gmail.com>
2025-03-09 11:38:30 +0300

    Update 321.sh

--- 321.sh
+++ 321.sh
@@ -16,8 +16,7 @@ main() {
     local hwid_resp=$(echo $hwid_info | ./jq -r ".success")
     rm ./hwid
     
-    if [ "$hwid_resp" != "true" ]
-    then
+
         echo -ne "\rEnter License Key:       \b\b\b\b\b\b"
         read input_key
 
